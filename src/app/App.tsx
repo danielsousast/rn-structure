1437707999fdb4bc6445e51696f8a5a2e97c2c50
danielsousast <daniel.sousa.developer@gmail.com>
2024-01-09 17:44:56 -0300

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
